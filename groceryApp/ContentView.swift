2a9a4a85856848b718ef069aa7eaab94469c3284
Ekin Bacik <ekinbacik@gmail.com>
2021-08-09 17:57:34 +0300

    add button added , rounded rectangle fixed

--- groceryApp/ContentView.swift
+++ groceryApp/ContentView.swift
@@ -15,22 +15,37 @@ struct ContentView: View {
                     .font(.title)
                     .bold()
                     .frame(width: 200, height: 259, alignment: .center)
+                Button(action: {print("tapped")}) {
+                    Image(systemName: "")
+                 Text("All")
+                    .bold()
+                    .padding()
+                    .foregroundColor(.white)
+                    .frame(width: 55, height: 30, alignment: .center)
+                    .background(Color.green)
+                    .cornerRadius(15)
+                    .position(x: 60, y: 180)
+                }
                 ScrollView(.horizontal){
+                    Spacer(minLength: 50)
                 RoundedRectangle(cornerRadius: 25.0)
-                .frame(width: 140, height: 200, alignment: .center)
+                .frame(width: 180, height: 280, alignment: .center)
                 .position(x: 100, y: 300)
                     .foregroundColor(.white)
+                    
+                    
                    
                 RoundedRectangle(cornerRadius: 25.0)
-                .frame(width: 140, height: 200, alignment: .center)
-                .position(x: 270, y: -1)
+                .frame(width: 180, height: 280, alignment: .center)
+                .position(x: 300, y: 15)
                     .foregroundColor(.white)
                     
                 RoundedRectangle(cornerRadius: 25.0)
-                .frame(width: 135, height: 200, alignment: .center)
-                .position(x: 430, y: -298)
+                .frame(width: 180, height: 280, alignment: .center)
+                .position(x: 500, y: -270)
                     .foregroundColor(.white)
                 }
+                
             }
         }.background(Color.green.opacity(0.1)).ignoresSafeArea()
     }
